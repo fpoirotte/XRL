0551d3778f269c93f2f278033b24503d2d602adb
François Poirotte <clicky@erebot.net>
2017-07-15 18:18:39 +0200

    Fix detection of associative arrays in ArrayType
    The "!=" operator returns true as long as both arrays share the same keys,
    despite them having totally different values. Hence the use of "!==" instead.

--- src/Types/ArrayType.php
+++ src/Types/ArrayType.php
@@ -47,9 +47,9 @@ class ArrayType extends \fpoirotte\XRL\Types\AbstractCollection
             sort($keys);
         }
 
-        // Hash / associative array.
-        if ($keys != $numeric) {
-            throw new \InvalidArgumentException('Expected array value2');
+        // Detect associative arrays (which are invalid for this type).
+        if ($keys !== $numeric) {
+            throw new \InvalidArgumentException('Expected an indexed array');
         }
 
         foreach ($value as $val) {
